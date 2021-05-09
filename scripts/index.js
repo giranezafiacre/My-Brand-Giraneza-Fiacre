8bf6296c7314d6493e6a04741319bf8001eda5ee
giranezafiacre <fiacregiraneza@gmail.com>
2021-05-09 16:47:17 +0200

    improving web view of contact page

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,7 @@
 var nav =document.querySelector('#botoes')
 window.onscroll=function(){
     "use strict"
-    if(document.body.scrollTop>=200||document.documentElement.scrollTop>=200){
+    if(document.body.scrollTop>=10||document.documentElement.scrollTop>=10){
         nav.style.backgroundColor="rgb(241, 168, 32)";
         nav.style.color="white";
     }else{
