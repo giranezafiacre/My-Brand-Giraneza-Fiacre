6e6eae89e8323fa577285878578914395a635268
unknown <fiacregiraneza@gmail.com>
2022-01-17 09:32:18 +0200

    improvement on pages

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,24 @@ var nav =document.querySelector('#botoes')
 window.onscroll=function(){
     "use strict"
     if(document.body.scrollTop>=10||document.documentElement.scrollTop>=10){
-        nav.style.backgroundColor="rgb(241, 168, 32)";
+        nav.style.backgroundColor="rgba(240, 240, 50)";
         nav.style.color="white";
     }else{
         nav.style.backgroundColor="transparent"
         
     }
 }
+const inViewport = (entries, observer) => {
+    entries.forEach(entry => {
+      entry.target.classList.toggle("is-inViewport", entry.isIntersecting);
+    });
+  };
+  
+  const Obs = new IntersectionObserver(inViewport);
+  const obsOptions = {}; //See: https://developer.mozilla.org/en-US/docs/Web/API/Intersection_Observer_API#Intersection_observer_options
+  
+  // Attach observer to every [data-inviewport] element:
+  const ELs_inViewport = document.querySelectorAll('[data-inviewport]');
+  ELs_inViewport.forEach(EL => {
+    Obs.observe(EL, obsOptions);
+  });
